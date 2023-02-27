9dccaceedc2d0a987cee85094b614b71e5cc1704
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-02-27 13:16:05 +0100

    Aggiungere i file di progetto.

---
+++ es.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void riempi(int[] a, int num, int max)
+            {
+                Random r = new Random();
+                for (int i = 0; i < num; i++)
+                {
+                    a[i] = (r.Next(100));
+                }
+            }
+            void scambio(int x, int y)
+            {
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+        }
+    }
+}
